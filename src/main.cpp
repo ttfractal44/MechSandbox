457dbdf991c0b7341be7ca15b75595a0b15150ca
ttfractal44 <ttfractal44@gmail.com>
2015-04-22 19:10:18 -0700

    Test of rotation and translation CopySequences, and nested CopySequences to create a grid

--- src/main.cpp
+++ src/main.cpp
@@ -40,27 +40,37 @@ int main() {
 	drawing1->addElement(union2);
 
 	Model::Drawing::Element* circle1 = drawing1->addElement(new Model::Drawing::Circle(osg::Vec2(0,0),0.1));
+	Model::Drawing::Element* circle2 = drawing1->addElement(new Model::Drawing::Circle(osg::Vec2(0,0),0.2));
+	Model::Drawing::Element* line1 = drawing1->addElement(new Model::Drawing::Line(osg::Vec2(-0.2,0),osg::Vec2(0.2,0)));
+
 
 	union2->addElement(circle1);
+	union2->addElement(circle2);
+	union2->addElement(line1);
 
 	Model::Drawing::CopySequence* sequence1 = (Model::Drawing::CopySequence*)drawing1->addElement(new Model::Drawing::CopySequence(union2));
 
+	Model::Drawing::CopySequence* sequence2 = (Model::Drawing::CopySequence*)drawing1->addElement(new Model::Drawing::CopySequence(sequence1));
+
 	printf("Setting properties\n");
-	sequence1->setTransformationProperties(50, osg::Vec2(0.5,0),M_PI/3,osg::Vec2(0,0));
+	//sequence1->setTransformationProperties(60, osg::Vec2(0,0),M_PI/30.0,osg::Vec2(0,0));
+	sequence1->setTransformationProperties(100, osg::Vec2(0.1,0),0.02,osg::Vec2(0,0));
+	sequence2->setTransformationProperties(100, osg::Vec2(0,0.1),0.01,osg::Vec2(0,0));
+
 
 	//union2->addElement(union2);
 
-	graphics->setSceneData(sequence1->osgnode);
+	graphics->setSceneData(sequence2->osgnode);
 
-	drawing1->update(100,100);
+	drawing1->update(1000000,100);
 
-	sequence1->setTransformationProperties(5, osg::Vec2(0.5,0),M_PI/3,osg::Vec2(0,0));
+	//sequence1->setTransformationProperties(5, osg::Vec2(0.5,0),M_PI/3,osg::Vec2(0,0));
 
-	drawing1->update(100,100);
+	//drawing1->update(100,100);
 
 	camera = graphics->getCamera();
 
-	camera->setViewMatrixAsLookAt(osg::Vec3(0,0,10),osg::Vec3(0,0,0),osg::Vec3(0,1,0));
+	camera->setViewMatrixAsLookAt(osg::Vec3(1,1,10),osg::Vec3(1,1,0),osg::Vec3(0,1,0));
 
 
 	client->main();
