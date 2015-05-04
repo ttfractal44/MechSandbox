ca54675cadf712119eb842c1d89c8bf2f0db30e6
ttfractal44 <ttfractal44@gmail.com>
2015-05-03 19:43:18 -0700

    Hack to GTK3 to allow a member function of an object to be passed as a callback to a GTK widget (cleaned up code)

--- src/misc/utils.cpp
+++ src/misc/utils.cpp
@@ -7,18 +7,6 @@
 
 #include "utils.h"
 
-/*
-// Library includes
-#include <SDL2/SDL.h>
-#include <gtk/gtk.h>
-#include <string>
-#include <vector>
-
-//Project includes
-#include "../misc/strings.h"
-*/
-
-
 std::string readResource(std::string path) {
 	SDL_RWops *file = SDL_RWFromFile((strings::resourcePath+path).c_str(), "r");
 	unsigned int size = file->size(file);
@@ -31,51 +19,3 @@ std::string readResource(std::string path) {
 
 	return contentsStr;
 }
-
-/*template <typename Type>
-class ClassMemberFunctionPtr {
-public:
-	ClassMemberFunctionPtr(Type* )
-private:
-	void (Type::*member)(void*);
-	Type* instance;
-};*/
-
-
-
-/*
-struct InstanceCallData {
-	void * object;
-	void * type;
-	void * (*method)();
-	void * data;
-};
-
-void call_instance_method(InstanceCallData* call_data) {
-	void * object = call_data->object;
-	void * type = call_data->type;
-	void * (*method)() = call_data->method;
-	//void * data = call_data->data;
-	printf("Calling method %p of object %p\n",method,object);
-	//(object->*method)();
-	//method();
-	//dynamic_cast<type>(object).*method();
-}
-
-void hack_g_signal_connect_classmethod(void * instance, const char * detailed_signal, void * type, void * object, void* (*method)(), void * data) {
-	// Hack to GTK3 to allow a member function of an object to be passed as a callback to a GTK widget
-
-	//g_signal_connect_swapped(gtkWindow, "configure-event", G_CALLBACK(configureCallback), this);
-
-	InstanceCallData* call_data = new InstanceCallData;
-	call_data->object = object;
-	call_data->type = type;
-	call_data->method = method;
-	call_data->data = data;
-
-	g_signal_connect_swapped(instance, detailed_signal, G_CALLBACK(call_instance_method), call_data);
-
-
-
-}
- */
--- src/misc/utils.h
+++ src/misc/utils.h
@@ -19,16 +19,8 @@
 
 // Exports
 std::string readResource(std::string path);
-/*template <typename Type>
-void hack_g_signal_connect_classmember(void * instance, const char * detailed_signal, void* object);*/
 
-/*template <typename Type>
-void some_templated_function();*/
-
-/*template <typename Type>
-void some_templated_function() {
-	printf("some_templated_function called\n");
-}*/
+// Implementations (cannot separate Delcaration and Implementation for functions using templates)
 
 template <typename Type>
 struct InstanceCallData {
@@ -42,24 +34,12 @@ void call_instance_member(InstanceCallData<Type>* call_data) {
 	Type* object = call_data->object;
 	void* (Type::*method)(void*) = call_data->method;
 	void* data = call_data->data;
-	printf("Calling method %p of object %p\n",method,object);
-	//(object->*method)();
-	//method();
-	//dynamic_cast<type>(object).*method();
 	(object->*method)(data);
 }
 
-//std::vector<int>* vec;
-
 template <typename Type>
-
-//                                                                                                  void (Editor::Editor::*)()
-// void hack_g_signal_connect_classmember(void * instance, const char * detailed_signal, Type* object, void* (Type::*method)()) { // THIS WORKS (1)
-//void hack_g_signal_connect_classmember(void * instance, const char * detailed_signal, Type* object, void* (Type::*method)(void*)) { // THIS WORKS (2)
 void hack_g_signal_connect_classmember(void * instance, const char * detailed_signal, Type* object, void* (Type::*method)(void*), void* data) { // THIS WORKS (3)
 	// Hack to GTK3 to allow a member function of an object to be passed as a callback to a GTK widget
-
-	//g_signal_connect_swapped(gtkWindow, "configure-event", G_CALLBACK(configureCallback), this);
 
 	InstanceCallData<Type>* call_data;
 	call_data = new InstanceCallData<Type>;
@@ -67,13 +47,8 @@ void hack_g_signal_connect_classmember(void * instance, const char * detailed_si
 	call_data->method = method;
 	call_data->data = data;
 
-	printf("hack_g_signal_connect_classmember: object is %p, method is %p\n", object, method);
-
 	g_signal_connect_swapped(instance, detailed_signal, G_CALLBACK(call_instance_member<Type>), call_data);
 
-
-
 }
-
 
 #endif /* UTILS_H_ */
