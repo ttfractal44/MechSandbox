fb06a82eff8b921a269647dea097c6bb8e0a2829
ttfractal44 <ttfractal44@gmail.com>
2015-05-24 15:53:10 -0700

    Line now references other point objects (instead of storing its own coordinates); Added debug printout of Line data

--- src/Model/Drawing/Line.cpp
+++ src/Model/Drawing/Line.cpp
@@ -10,25 +10,33 @@
 namespace Model {
 namespace Drawing {
 
-Line::Line(osg::Vec2 _pos1, osg::Vec2 _pos2) {
-	pos1 = _pos1;
-	pos2 = _pos2;
+Line::Line(Point* _point1, Point* _point2) {
+	point1 = _point1;
+	point2 = _point2;
 	drawelements->setMode(osg::PrimitiveSet::LINE_STRIP);
-
+	dependOn(point1);
+	dependOn(point2);
+	instanceclassname = "Line";
 }
 
 Line::~Line() {
 	// TODO Auto-generated destructor stub
 }
 
-void Line::updateImpl(uint depth, uint resolution) {
+void Line::updateImpl(uint resolution) {
 	verts->clear();
-	verts->push_back(pos1);
-	verts->push_back(pos2);
+	verts->push_back(point1->pos);
+	verts->push_back(point2->pos);
 	drawelements->clear();
 	drawelements->push_back(0);
 	drawelements->push_back(1);
-	//osggeom->dirtyDisplayList();
+	osggeom->dirtyDisplayList();
+}
+
+std::string Line::printAttributes() {
+	char str[128];
+	sprintf(str, "(%f,%f) to (%f,%f)", point1->pos._v[0], point1->pos._v[1], point2->pos._v[0], point2->pos._v[1]);
+	return std::string(str);
 }
 
 } /* namespace Drawing */
--- src/Model/Drawing/Line.h
+++ src/Model/Drawing/Line.h
@@ -9,17 +9,19 @@
 #define LINE_H_
 
 #include "Curve.h"
+#include "Point.h"
 
 namespace Model {
 namespace Drawing {
 
 class Line: public Curve {
 public:
-	Line(osg::Vec2 _pos1, osg::Vec2 _pos2);
+	Line(Point* point1, Point* point2);
 	virtual ~Line();
-	osg::Vec2 pos1;
-	osg::Vec2 pos2;
-	void updateImpl(uint depth, uint resoluton);
+	Point* point1;
+	Point* point2;
+	void updateImpl(uint resoluton);
+	std::string printAttributes();
 };
 
 } /* namespace Drawing */
